36bf7847863efff8f7b020fdaf466b1f8f3c44e9
Phong <thanhphong9718@gmail.com>
2021-03-05 23:06:30 +0700

    fix create info

--- service/admin/information/create.go
+++ service/admin/information/create.go
@@ -32,7 +32,7 @@ func CreateInformationByRequest(db *sql.DB, address, phone, email, maps, title, 
 		(address, email, phone, maps, title, description, img)
 	VALUES($1, $2, $3, $4, $5, $6, $7);
 	`
-	_, err := db.Exec(query, address, phone, email, maps, title, description, img)
+	_, err := db.Exec(query, address, email, phone, maps, title, description, img)
 	if err != nil {
 		logrus.WithFields(logrus.Fields{}).Errorf("[CreateInformationByRequest]Insert contact DB err  %v", err)
 		return errors.New("Lỗi hệ thống, vui lòng thử lại")
