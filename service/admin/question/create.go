e871887ad4f3931247e2f7abdcc1a39cfb0c816e
Phong <thanhphong9718@gmail.com>
2021-04-11 18:03:31 +0700

    update endpoint create a question

--- service/admin/question/create.go
+++ service/admin/question/create.go
@@ -8,9 +8,9 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func (tc StoreQuestion) CreateQuestion(name, answerA, answerB, answerC, answerD, img, result string, liet bool, typeQ string) (response.MessageResponse, error) {
+func (tc StoreQuestion) CreateQuestion(name, answerA, answerB, answerC, answerD, img, result string, liet bool) (response.MessageResponse, error) {
 	resp := response.MessageResponse{}
-	if err := CreateQuestionByRequest(tc.db, name, answerA, answerB, answerC, answerD, img, result, liet, typeQ); err != nil {
+	if err := CreateQuestionByRequest(tc.db, name, answerA, answerB, answerC, answerD, img, result, liet); err != nil {
 		logrus.WithFields(logrus.Fields{}).Errorf("[CreateQuestion]Insert Question DB err  %v", err)
 		return resp, err
 	}
@@ -19,14 +19,14 @@ func (tc StoreQuestion) CreateQuestion(name, answerA, answerB, answerC, answerD,
 	return resp, nil
 }
 
-func CreateQuestionByRequest(db *sql.DB, name, answerA, answerB, answerC, answerD, img, result string, liet bool, typeQ string) error {
+func CreateQuestionByRequest(db *sql.DB, name, answerA, answerB, answerC, answerD, img, result string, liet bool) error {
 	query := `
 	INSERT INTO question
-		(name, anwser_correct, paralysis, answera, answerb, answerc, answerd, img, question_type)
+		(name, anwser_correct, paralysis, answera, answerb, answerc, answerd, img)
 	VALUES
-		($1, $2, $3, $4, $5, $6, $7, $8, $9);
+		($1, $2, $3, $4, $5, $6, $7, $8);
 	`
-	_, err := db.Exec(query, name, result, liet, answerA, answerB, answerC, answerD, img, typeQ)
+	_, err := db.Exec(query, name, result, liet, answerA, answerB, answerC, answerD, img)
 	if err != nil {
 		logrus.WithFields(logrus.Fields{}).Errorf("[CreateInformationByRequest]Insert Question DB err  %v", err)
 		return errors.New("Lỗi hệ thống, vui lòng thử lại")
